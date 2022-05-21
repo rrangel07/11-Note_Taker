5391c90073ada9693c2f57d51e9199bd67179aef
rrangel07 <99489197+rrangel07@users.noreply.github.com>
2022-05-21 16:25:02 -0400

    Still developing routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,36 +1,40 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('..helpers/fsUtils');
-const uuid = require('..helpers/uuid');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 notes.get('/', (req,res) => {
     console.info(`${req.method} request received for notes`);
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 notes.post('/', (req,res) => {
     console.info(`${req.method} request received for notes`);
+    console.log(req.body)
     const { tittle, text } = req.body;
+    console.log(tittle,text);
     if(req.body) {
         const newNote = {
             tittle,
             text,
             id: uuid(),
         }
-        readAndAppend (newNote, '../db/db.json');
+        readAndAppend (newNote, './db/db.json');
         res.json(`Note added to database 🚀`);
     } else {
         res.error(`Error saving the note`);
     }
 });
 
-notes.delete('/:id', (req,res) => {
-    // console.info(`${req.method} request received for notes`);
+// notes.delete('/:id', (req,res) => {
+//     console.info(`${req.method} request received for notes`);
 
-    // readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
-    //     res.json(`Note added to database 🚀`);
-    // } else {
-    //     res.error(`Error adding the `)
-    // }
-});
+//     readFromFile('../db/db.json').then((data) => {
+//         const notesArray = (JSON.parse(data)).filter(each => each.id != req.params.id)
+//     });
+//     //     res.json(`Note added to database 🚀`);
+//     // } else {
+//     //     res.error(`Error adding the `)
+//     // }
+// });
 
 module.exports = notes;
